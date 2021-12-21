19a17a9edf3ff941bd8058b0f1f64798c8debea4
Joan Company <1637908@uab.cat>
2021-12-21 21:42:28 +0100

    versió final

--- practica4.c
+++ practica4.c
@@ -6,16 +6,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-//int ignorar(){
-//	if (pointer in){ escriure les 4 condicions
-	
-	
-//}
-
-void read(int row, int col, char *filename){
-	FILE *fitxer;
-	fitxer = open("CAVideos.csv", 0666)
-	
 char *getField( int fn, char line[], char field[] ){
 	int i;
 	char *p, aux[256];
@@ -26,13 +16,226 @@ char *getField( int fn, char line[], char field[] ){
 	return strcpy(field,p);
 }
 
-//int read(int fitxer, void *buffer, int 5){
-	//while(fitxer != EOF){
-		//printf(
 
 int main(int argc, char** argv){
-	int fitxer;
-	//FILE *fitxer 
-	fitxer = open ("CAvideos.csv", 0666 );
+	int fitxer,i=0, col=1,pointer;
+	int top_l=0, top_dl=0;
+	int top1=0, top2=0, top3=0, aux=0, aux1=0,aux2=0;
+	float v=0,l=0,dl=0; //visualizaciones, likes, dislikes
+	char c;
+	char sortida1[10000],sortida2[10000],sortida3[10000]; //a la funció getField l'array sortida es guarda el camp que volem
+	char views[1000], likes[1000], dislikes[1000];
+	char buf[10000];//buf és un guardem la sortida
+	char topl_buf[10000],topdl_buf[10000];
+	char top_buf1[10000],top_buf2[10000],top_buf3[10000];
 	
+	fitxer = open("CAvideos.csv", 0666 );
+
+	while ((pointer = read(fitxer, &c, 1)) > 0){
+
+		if(c != '\n'){
+			if (c == ','){
+				col++;	
+				if (col != 12 && col <= 15){
+					buf[i] = c;
+					i++;
+				}
+			}
+			else{
+				if (col != 12 && col <= 15){
+					buf[i] = c;
+					i++;
+				}
+			}
+		
+		}
+		else{
+				buf[i]='\n'; //per saber final línia, bno 0 = buit
+				getField(11, buf, sortida1);
+				getField(12, buf, sortida2);
+				getField(13, buf, sortida3);
+				if (((strcmp(sortida1, "True")==0 || strcmp(sortida1,"False")==0)) || (((strcmp(sortida2, "False"))==0) && ((strcmp(sortida3, "False"))==0))){
+
+					//printf("%s", buf);	
+				
+					getField(7, buf, views);
+					v += atoi(views);
+					if (atoi(views)>top1){
+						aux = top2;
+						top3 = aux;
+						aux1 = top1;
+						top2 = aux1;
+						top1 = atoi(views);
+						
+						 for (int i=0; i<sizeof(buf); i++){
+							top_buf1[i] = buf[i];	
+						}
+						
+					}
+					else{
+						if(top2<atoi(views)){
+							aux2 = top2;
+							top3= aux2;
+							top2=atoi(views);
+							
+							for (int i=0; i<sizeof(buf); i++){
+								top_buf2[i] = buf[i];	
+							}
+						}
+						else{
+							if(top3<atoi(views)){
+								top3 = atoi(views);
+								
+								for (int i=0; i<sizeof(buf); i++){
+									top_buf3[i] = buf[i];	
+								}
+					
+							}
+						}
+					}
+							
+					getField(8, buf, likes);
+					l += atoi(likes);
+					if(atoi(likes)>top_l){
+						top_l = atoi(likes);
+						for (int i=0; i<sizeof(buf); i++){
+							topl_buf[i] = buf[i];	
+						}
+					}
+					getField(9, buf, dislikes);
+					dl += atoi(dislikes);
+					if(atoi(dislikes)>top_dl){
+						top_dl = atoi(dislikes);
+						for (int i=0; i<sizeof(buf); i++){
+							topdl_buf[i] = buf[i];
+						}
+					}
+				}
+			
+			memset(buf, 0, sizeof(buf)); //reinicialitzar buf
+			i = 0;
+			col = 1;
+			//}
+		}
+		
+	}
+	const char *mit1= "La mitjana de visualitzacions és:";
+	v = v/35585;
+	char vv[100];
+	gcvt(v, 13, vv);
+	const char *mit2= "La mitjana de likes és:";
+	l = l/35585;
+	char ll[100];
+	gcvt(l, 13, ll);
+	const char *mit3= "La mitjana de dislikes és:";
+	dl = dl/35585;
+	char dldl[100];
+	gcvt(dl, 13, dldl);
+	
+	//printf("visualitzacions: %f, likes: %f,dislikes: %f", v,l,dl);
+	
+	const char *mit4= "El video amb més likes és:";
+	char titol[10000], publish_time[1000], num_v[100], lk[100], dlk[100];
+	getField(2, topl_buf, titol);
+	getField(5, topl_buf, publish_time);
+	getField(7, topl_buf, num_v);
+	getField(8, topl_buf, lk);
+	getField(9, topl_buf, dlk);
+	
+	const char *mit5= "El video amb més dislikes és:";
+	char titol1[10000], publish_time1[1000], num_v1[100], lk1[100], dlk1[100];
+	getField(2, topdl_buf, titol1);
+	getField(5, topdl_buf, publish_time1);
+	getField(7, topdl_buf, num_v1);
+	getField(8, topdl_buf, lk1);
+	getField(9, topdl_buf, dlk1);
+	
+	
+	//printf("Top likes: %s, %s, %s,%s,%s", titol, publish_time, num_v, lk,dlk);
+	//printf("Top dislikes: %s, %s, %s, %s,%s", titol1, publish_time1, num_v1, lk1,dlk1);
+	
+	const char *mit6= "El vídeo amb més visualitzacions és:";
+	char t1[10000], v1[5000];
+	getField(2, top_buf1, t1);
+	getField(7, top_buf1, v1);
+	const char *mit7= "El segon vídeo amb més visualitzacions és:";
+	char t2[10000], v2[5000];
+	getField(2, top_buf2, t2); 
+	getField(7, top_buf2, v2);
+	const char *mit8= "El tercer vídeo amb més visualitzacions és:";
+	char t3[10000], v3[5000];
+	getField(2, top_buf3, t3); 
+	getField(7, top_buf3, v3);
+	
+	//printf("Top 1: %s, %s", t1,v1);
+	//printf("Top 2: %s, %s", t2,v2);
+	//printf("Top 3: %s, %s", t3,v3);
+	
+	int resultat;
+	resultat=creat("GlobalOutput.txt", 0666);
+	const char *y = "\n";
+	const char *z = " ";
+	
+	write(resultat,mit1,strlen(mit1));
+	write(resultat,y,strlen(y));
+	write(resultat,vv,7);
+	write(resultat,y,strlen(y));
+	write(resultat,mit2,strlen(mit2));
+	write(resultat,y,strlen(y));
+	write(resultat,ll, 12);
+	write(resultat,y,strlen(y));
+	write(resultat,mit3,strlen(mit3));
+	write(resultat,y,strlen(y));
+	write(resultat,dldl,13);
+	write(resultat,y,strlen(y));
+	
+	write(resultat,mit6,strlen(mit6));
+	write(resultat,y,strlen(y));
+	write(resultat,t1,52);
+	write(resultat,z,strlen(z));
+	write(resultat,v1,9);
+	write(resultat,y,strlen(y));
+	
+	write(resultat,mit7,strlen(mit7));
+	write(resultat,y,strlen(y));
+	write(resultat,t2,57);
+	write(resultat,z,strlen(z));
+	write(resultat,v2,8);
+	write(resultat,y,strlen(y));
+	
+	write(resultat,mit8,strlen(mit8));
+	write(resultat,y,strlen(y));
+	write(resultat,t3,38);
+	write(resultat,z,strlen(z));
+	write(resultat,v3,8);
+	write(resultat,y,strlen(y));
+	
+	write(resultat,mit4,strlen(mit4));
+	write(resultat,y,strlen(y));
+	write(resultat, titol, 47);
+	write(resultat,z,strlen(z));
+	write(resultat,publish_time,18);
+	write(resultat,z,strlen(z));
+	write(resultat,num_v,8);
+	write(resultat,z,strlen(z));
+	write(resultat,lk,7);
+	write(resultat,z,strlen(z));
+	write(resultat,dlk,6);
+	write(resultat,y,strlen(y));
+	
+	write(resultat,mit5,strlen(mit5));
+	write(resultat,y,strlen(y));
+	write(resultat, titol1, 52);
+	write(resultat,z,strlen(z));
+	write(resultat,publish_time1,18);
+	write(resultat,z,strlen(z));
+	write(resultat,num_v1,9);
+	write(resultat,z,strlen(z));
+	write(resultat,lk1,7);
+	write(resultat,z,strlen(z));
+	write(resultat,dlk1,6);
+	
+	
+	close(resultat);
+							
 }
